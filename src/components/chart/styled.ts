042323d4c2dfd0159349131a3f6c50b0843a522c
jorgejunior618 <jorgejunior618@gmail.com>
2022-06-10 13:25:06 -0300

    Refatoração de nomenclatura do componente do gráfico, e recebimento de dados

---
+++ src/components/chart/styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const ChartWrapper = styled.section`
+  background-color: white;
+  padding: 10px 8px;
+  border-radius: 10px;
+  box-shadow: 0 0 5px ${({ theme }) => theme.colors.shadowColor};
+`;
