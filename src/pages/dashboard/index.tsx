cb0c7a5be4150bddb34b301d9171fb83362666db
jorgejunior618 <jorgejunior618@gmail.com>
2022-06-11 12:40:44 -0300

    Tratamento dos dados para mostrar nos gráficos

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,51 @@
-import Graphic from '../../components/graphic';
-import { DashboardWrapper, GraphicsArea } from './styled';
+import Chart from '../../components/chart';
+import { DashboardWrapper, ChartsArea } from './styled';
+import movieData from '../../movie-data.json';
+import { useEffect, useState } from 'react';
 
 function Dashboard() {
+  const [balances, setBalances] = useState<number[]>([]);
+  const [movies, setMovies] = useState<string[]>([]);
+
+  const [balancesDates, setBalancesDates] = useState<
+    {
+      name: string;
+      data: [number, number][];
+    }[]
+  >([]);
+
+  useEffect(() => {
+    const balanceList: number[] = [];
+    const moviesList: string[] = [];
+    const balanceListDates: {
+      name: string;
+      data: [number, number][];
+    }[] = [];
+
+    movieData.movies.forEach((movie) => {
+      let total = 0;
+      const data: [number, number][] = [];
+      movie.balances.forEach((balance) => {
+        total += balance.balance;
+        data.push([new Date(balance.date).getTime(), balance.balance]);
+      });
+
+      balanceListDates.push({
+        name: movie.name,
+        data,
+      });
+
+      balanceList.push(Number(total.toFixed(2)));
+      moviesList.push(movie.name);
+    });
+
+    console.log({ balanceListDates });
+
+    setBalances(balanceList);
+    setMovies(moviesList);
+    setBalancesDates(balanceListDates);
+  }, []);
+
   return (
     <DashboardWrapper>
       <header>
@@ -9,10 +53,22 @@ function Dashboard() {
         <h2>Desafio Técnico Frontend</h2>
       </header>
 
-      <GraphicsArea>
-        <Graphic />
+      <ChartsArea>
+        <Chart
+          title="Bilheteria Arrecadada (em US$ milhões)"
+          data={balances}
+          categories={movies}
+          chartType="bar"
+          dataName="Quantidade"
+        />
+        <Chart
+          title="Bilheteria (Levantamento periódico)"
+          dataScatter={balancesDates}
+          chartType="scatter"
+          dataName="Quantidade"
+        />
         <div></div>
-      </GraphicsArea>
+      </ChartsArea>
     </DashboardWrapper>
   );
 }
